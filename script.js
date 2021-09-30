5c91aa0160c803abef55c40048147c2eda6ee69f
ianquill <ian@ianquill.com>
2021-09-29 18:42:14 -0700

    Implemented alternating players and square-filling logic

--- script.js
+++ script.js
@@ -9,12 +9,9 @@ let gameBoard = (function() {
             board[i] = [];
         }
 
-        let h = 0;
-
         for (let i = 0; i < width; i++) {
             for (let j = 0; j < height; j++) {
-                board[i][j] = h++;
-                
+                board[i][j] = "empty";
             }
             
         }
@@ -23,20 +20,45 @@ let gameBoard = (function() {
 
     }
 
+    function checkCell (i, j) {
+        // check cell position at board[i][j] if it has a value stored
+        if (board[i][j] === "empty") {
+            console.log(gameController.getCurrentPlayer());
+            board[i][j] = gameController.getCurrentPlayer();
+            console.log("recorded " + gameController.getCurrentPlayer());
+        } else {
+            return;
+        }
+    }
+
     // clear board 
-    return { createBoard, board };
+    return { createBoard, checkCell, board };
 
 })();
 
 let displayController = (function() {
+    'use strict'
+
     const gameContainer = document.querySelector(".game-container");
 
-    function displayBoard() {
+    function refreshBoard() {
 
+        clearBoard();
         for (let i = 0; i < gameBoard.board.length; i++) {
             for (let j = 0; j < gameBoard.board[i].length; j++) {
                 const cell = document.createElement('div');
                 cell.classList.add("cell");
+                
+                if (gameBoard.board[i][j] === "player1") {
+                    cell.textContent = "O";
+                } else if (gameBoard.board[i][j] === "player2") {
+                    cell.textContent = "X";
+                }
+
+                cell.addEventListener('click', () => {
+                    gameController.clickCell(i, j);
+                })
+
                 gameContainer.appendChild(cell);
                 
             }
@@ -45,19 +67,59 @@ let displayController = (function() {
         
     }
 
-    return {displayBoard};
+    function clearBoard() {
+        while (gameContainer.firstChild) {
+            gameContainer.removeChild(gameContainer.firstChild);
+        }
+    };
+
+    return {refreshBoard};
 
 })();
 
-let gameController = (function() {
-
-})();
-
-let Player = (name) => {
+const Player = (name) => {
 
 }
 
-console.log(gameBoard.createBoard(3, 3));
-console.log("hello");
+const player1 = Player("Ian");
+const player2 = Player("Eleanor");
 
-displayController.displayBoard();
+const gameController = (function() {
+    'use strict'
+
+    let currentPlayer = "player1";    // eventually add a function that randomizes this starting value
+
+    function clickCell(i, j) {
+        gameBoard.checkCell(i, j);
+        displayController.refreshBoard();
+        switchPlayer();
+        // check gameBoard for the specific cell
+            // displayController.updateBoard();
+            // if legal, displayController is updated
+            // and update gameController to check for win condition
+    }
+    
+    function switchPlayer() {
+        if (currentPlayer === "player1") {
+            currentPlayer = "player2";
+            console.log("changed to player2")
+            console.log(currentPlayer);
+        } else if (currentPlayer === "player2") {
+            currentPlayer = "player1";
+        }
+        console.log("current player: ", gameController.currentPlayer);
+        console.log("other current player: ", currentPlayer);
+    }
+
+    function getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    return {clickCell, getCurrentPlayer}
+
+})();
+
+
+console.log(gameBoard.createBoard(3, 3));
+
+displayController.refreshBoard();
